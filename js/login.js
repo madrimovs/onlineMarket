1e7ad2f9e791f1553ea61c511e7264fe0ffd757b
MadrimovS <sardormadrimov516@gmail.com>
2023-02-10 15:54:50 +0500

    main

---
+++ js/login.js
@@ -0,0 +1,36 @@
+import findElement from "./helpers/findElement.js";
+import { BASE_URL } from "./app.js";
+
+const elForm = findElement("#form");
+const elEmail = findElement("#email");
+const elPassword = findElement("#password");
+const errorText = findElement("#errorText");
+const errorPas = findElement("#errorPas");
+
+errorText.style.display = "none";
+errorPas.style.display = "none";
+
+///////////////////// GENERATE ERROR TEXT FUNCTION //////////////////////
+function generateError(element, text) {
+	element.textContent = text;
+	element.style.display = "block";
+
+	const timer = setTimeout(() => {
+		element.style.display = "none";
+
+		clearTimeout(timer);
+	}, 3000);
+}
+
+/////////////////////////////// LOGIN ELFORM ///////////////////////////////
+elForm.addEventListener("submit", (evt) => {
+	evt.preventDefault();
+
+	if (elEmail.value.length === 0) {
+		generateError(errorText, "Iltimos e-mailingizni kiriting");
+	}
+
+	if (elPassword.value.length < 6) {
+		generateError(errorPas, "Parol 6 ta belgidan kam bo'lmasligi kerak");
+	}
+});
